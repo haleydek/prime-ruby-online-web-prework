0195d621f48b7b52b600fd4f4faee134102138c8
Haley DeKeyser <hcd09@live.com>
2019-05-11 16:03:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,6 @@ def prime?(n)
   return false if n < 2
   test_range = [2..Math.sqrt(n).round]
   for a in test_range
-    if n % a == 0
-      return false
+    n % a == 0 ? false : true
   end
 end
